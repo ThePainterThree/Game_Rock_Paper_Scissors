076a0d87a462e98bad020ce6aaca04b9fd005ed7
Patricia <marie.lou.trips@gmail.com>
2024-02-26 23:50:48 +0100

    🎨 improved code readabilitv

--- js/game.js
+++ js/game.js
@@ -1,11 +1,9 @@
 function submitName() {
     const userName = document.getElementById('name').value.trim();
-    //const nameForm = document.querySelector('.game-form');
     const playerName = document.getElementById('player-name');
 
     if (userName !== '') {
         localStorage.setItem('userName', userName);
-        //nameForm.style.display = 'none';
         document.querySelector('.game-form').style.display ='none';
         
         playerName.textContent = `Hello, ${userName}!`;
@@ -15,8 +13,6 @@ function submitName() {
         alert('Please enter a username.');
     }
 }
-
-
 
 document.getElementById('rockButton').addEventListener('click', function() {
     selectMove('Rock');
@@ -39,37 +35,43 @@ document.getElementById('spockButton').addEventListener('click', function() {
 });
 
 
-function selectMove(move) {
+function selectMove(playerMove) {
     const userName = localStorage.getItem('userName');
-    const computerOptions = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
-    const computerPlay = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+    const computerOptions = ['Rock', 'Paper', 'Scissors' , 'Lizard', 'Spock'];
+    const computerMove = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
     const playerName = document.getElementById('player-name');
     playerName.textContent = `Hello, ${userName}!`;
 
     const computerChoiceElement = document.getElementById('computerChoice');
-    computerChoiceElement.textContent = `Computer played: ${computerPlay}`;
+    computerChoiceElement.textContent = `Computer played: ${computerMove}`;
 
+    const winningLogic = {
+        'Scissors': ['Paper', 'Lizard'],
+        'Paper': ['Rock', 'Spock'],
+        'Rock': ['Lizard', 'Scissors'],
+        'Lizard': ['Spock', 'Paper'],
+        'Spock': ['Rock', 'Scissors']
+    }
+    
+    const getGameOptions = document.querySelector('.game-options');
+
+    getGameOptions.addEventListener('click', function(event){
+        const target = event.target;
+        if(target.tagName === 'BUTTON'){
+            const playerMove = target.textContent
+        }
+   
     let winnerMessage;
-    if (move === computerPlay) {
+    if (playerMove === computerMove) {
         winnerMessage = "It's a tie!";
-    } else if (
-        (move === 'Scissors' && computerPlay === 'Paper') ||
-        (move === 'Paper' && computerPlay === 'Rock') ||
-        (move === 'Rock' && computerPlay === 'Lizard') ||
-        (move === 'Lizard' && computerPlay === 'Spock') ||
-        (move === 'Spock' && computerPlay === 'Scissors') ||
-        (move === 'Scissors' && computerPlay === 'Lizard') ||
-        (move === 'Lizard' && computerPlay === 'Paper') ||
-        (move === 'Paper' && computerPlay === 'Spock') ||
-        (move === 'Spock' && computerPlay === 'Rock') ||
-        (move === 'Rock' && computerPlay === 'Scissors')
-    ) {
+    } else if (winningLogic[playerMove].includes(computerMove)){
         winnerMessage = `${userName} wins!`;
-        //console.log(userName);
     } else {
         winnerMessage = 'Computer wins!';
     }
     const winnerMessageElement = document.getElementById('winnerMessage');
     winnerMessageElement.textContent = winnerMessage;
+
+})
 }
