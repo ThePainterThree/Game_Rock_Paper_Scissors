419c9f404a154c648192bfb1972a3f39be5aac71
Patricia <marie.lou.trips@gmail.com>
2026-06-14 00:57:16 +0200

    Add functions: - to get a computerMove - and to determine the winner

--- src/helpers/gameLogic.ts
+++ src/helpers/gameLogic.ts
@@ -1,4 +1,4 @@
-import type { PlayerMove } from "../types/game";
+import type { GameResult, PlayerMove } from "../types/game";
 
 export const moves: PlayerMove[] = [
   "rock",
@@ -9,9 +9,27 @@ export const moves: PlayerMove[] = [
 ];
 
 export const winningLogic: Record<PlayerMove, PlayerMove[]> = {
- scissors: ["paper", "lizard"],
- paper: ["rock", "spock"],
- rock: ["lizard", "scissors"],
- spock: ["scissors", "rock"],
- lizard: ["spock", "paper"]
+  scissors: ["paper", "lizard"],
+  paper: ["rock", "spock"],
+  rock: ["lizard", "scissors"],
+  spock: ["scissors", "rock"],
+  lizard: ["spock", "paper"],
+};
+
+export function getRandomComputerMove(): PlayerMove {
+  const randomIndex = Math.floor(Math.random() * moves.length);
+  return moves[randomIndex];
+}
+
+export function andTheWinnerIs(
+  playerMove: PlayerMove,
+  computerMove: PlayerMove,
+): GameResult {
+  if (playerMove === computerMove) {
+    return "draw";
+  }
+  if (winningLogic[playerMove].includes(computerMove)) {
+    return "win";
+  }
+  return "lose";
 }
