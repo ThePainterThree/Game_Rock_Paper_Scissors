a55b464c54e9a04d08a22178e055da4ef4172c97
Patricia <marie.lou.trips@gmail.com>
2026-06-14 18:43:09 +0200

    format winning message

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,11 @@ function App() {
     setGameResult(result);
   }
 
+  function displayMove(move: PlayerMove | null) {
+    if (!move) return "";
+    return move.charAt(0).toUpperCase + move.slice(1);
+  }
+
   return (
     <div>
       {moves.map((move) => (
@@ -29,12 +34,18 @@ function App() {
         </Button>
       ))}
 
-      {playerMove && (
-        <>
-          <p>You played: {playerMove}</p>
-          <p>Computer played: {computerMove}</p>
-          <p>Result: {gameResult}</p>
-        </>
+      {playerMove && computerMove && gameResult && (
+        <section>
+          <p>You played: {displayMove(playerMove)}</p>
+          <p>Computer played: {displayMove(computerMove)}</p>
+          <h2>
+            {gameResult === "win"
+              ? "You won!"
+              : gameResult === "lose"
+                ? "Computer won!"
+                : "It's a draw!"}
+          </h2>
+        </section>
       )}
     </div>
   );
