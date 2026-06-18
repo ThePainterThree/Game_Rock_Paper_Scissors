579762b9a2ce7cd900095fb36f133f45b1f57018
Patricia <marie.lou.trips@gmail.com>
2026-06-18 23:35:08 +0200

    Add type Score (for LeaderBoard - wip)

--- src/types/game.ts
+++ src/types/game.ts
@@ -1,3 +1,9 @@
 export type PlayerMove = "rock" | "paper" | "scissors" | "lizard" | "spock";
 
 export type GameResult = "win" | "lose" | "draw";
+
+export type Score = {
+    player: number,
+    computer: number,
+    draws: number
+}
